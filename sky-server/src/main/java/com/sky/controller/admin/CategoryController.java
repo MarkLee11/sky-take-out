b7f961b59a4f397cf3ec6746906b3431fd7a128a
Mark <aa1439422778@gmail.com>
2024-11-23 21:08:56 +0800

    菜品阶段-批量删除菜品阶段

--- sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -104,4 +104,6 @@ public class CategoryController {
         List<Category> list = categoryService.list(type);
         return Result.success(list);
     }
+
+
 }
--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.sky.entity.DishFlavor;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -11,4 +12,9 @@ public interface DishFlavorMapper {
 
 
     void insertBatch(List<DishFlavor> flavors);
+
+    @Delete("delete from dish_flavor where dish_id=#{dishId}")
+    void deleteByDishId(Long dishId);
+
+    void deleteByDishIds(List<Long> dishIds);
 }
